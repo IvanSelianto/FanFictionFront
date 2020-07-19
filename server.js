a97915a79e29486905a7c86847f79f9be05534b3
IvanSelianto <ivan.selianto1@gmail.com>
2020-07-20 00:29:21 +0300

    13 commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + "/src"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "src", "index.html"));
 });

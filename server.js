de031368834ef2f06d5af8f96cd9b35ebfe214ad
IvanSelianto <ivan.selianto1@gmail.com>
2020-07-19 23:33:39 +0300

    Ninth commit

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 app.use(express.static(__dirname));
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + 'src/index.html'))
-
-})
-app.listen(process.env.PORT||8080)
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+app.listen(process.env.PORT || 8080)
 
 

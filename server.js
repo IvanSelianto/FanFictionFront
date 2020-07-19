586d82000663e23e6bb4ad9f62aad302499b0584
IvanSelianto <ivan.selianto1@gmail.com>
2020-07-20 01:22:59 +0300

    17 commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 app.use(express.static(__dirname + "/dist/fanfictionAngularFront"));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname+'/dist/fanFictionAngularFront/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/fanfictionAngularFront/index.html'));
 });
 app.listen(process.env.PORT || 8080)
 
